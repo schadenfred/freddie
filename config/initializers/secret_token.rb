793a46328010b66ac427a495011f3722a775787e
Fred Schoeneman <fred.schoeneman@gmail.com>
2010-10-13 06:04:56 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freddie::Application.config.secret_token = '4d9f0f5cae76861dd03c6bf637247c9595ee4e8724932b1a1d1eaab465bb5e3c23270a8fd99ef90c1910f00ac11b2fcd94e2f9bb2c29da90a456ab8c6cebb46a'
