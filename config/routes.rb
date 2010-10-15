a2248c54a3283584d78811d6f70e631be1d14d25
Fred Schoeneman <fred.schoeneman@gmail.com>
2010-10-15 01:26:13 -0700

    google apps verification

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Freddie::Application.routes.draw do
+  get "home/index"
+  root :to => "home#index"
+
+  resources :posts
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -56,3 +61,4 @@ Freddie::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 end
+
